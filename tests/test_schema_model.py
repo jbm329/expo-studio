bd24b0d33fda6151bb21840e794d53f4ce571e64
jbm329 <jonas.brannstrom@gmail.com>
2026-09-14 14:47:45 +0200

    test: update test_schema_model after refactor

--- tests/test_schema_model.py
+++ tests/test_schema_model.py
@@ -1,31 +1,43 @@
+from __future__ import annotations
+
 from expo_jbm329.services.schema_model import build_schema_dict
+
 
 def test_build_schema_dict_empty():
     assert build_schema_dict({}) == {"tables": {}, "by_schema": {}}
 
-def test_build_schema_dict_with_data():
+
+def test_build_schema_dict_uses_table_and_view_metadata():
     cache = {
         "tables": [{"schema": "dbo", "name": "Users"}],
         "views": [{"schema": "dbo", "name": "ActiveUsers"}],
         "columns": {
-            ("dbo", "Users"): [{"COLUMN_NAME": "id"}, {"name": "email"}],
-            ("dbo", "ActiveUsers"): [{"ColumnName": "id"}]
-        }
+            ("dbo", "Users"): [
+                {"COLUMN_NAME": "id"},
+                {"name": "email"},
+            ],
+            ("dbo", "ActiveUsers"): [
+                {"ColumnName": "id"},
+            ],
+        },
     }
+
     res = build_schema_dict(cache)
-    
-    assert "dbo.Users" in res["tables"]
+
     assert res["tables"]["dbo.Users"] == ["id", "email"]
     assert res["tables"]["dbo.ActiveUsers"] == ["id"]
-    
-    assert "dbo" in res["by_schema"]
-    assert "Users" in res["by_schema"]["dbo"]
     assert res["by_schema"]["dbo"]["Users"] == ["id", "email"]
+    assert res["by_schema"]["dbo"]["ActiveUsers"] == ["id"]
 
-def test_build_schema_dict_missing_info():
+
+def test_build_schema_dict_skips_invalid_rows():
     cache = {
-        "tables": [{"schema": "dbo"}], # missing name
-        "columns": {}
+        "tables": [{"schema": "dbo"}, {"name": "Users"}],
+        "views": [],
+        "columns": {},
     }
+
     res = build_schema_dict(cache)
-    assert res["tables"] == {}
+
+    assert res == {"tables": {}, "by_schema": {}}
+
